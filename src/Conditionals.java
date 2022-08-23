d22a066aaf5470047c674a477804d129c477098e
Gustavo Lira <gu.lirar23@gmail.com>
2022-08-23 12:12:01 -0500

    add an example of conditionals

---
+++ src/Conditionals.java
@@ -0,0 +1,10 @@
+public class Conditionals {
+    public static void main(String[] args){
+        int number = 18;
+        if(number >= 20){
+            System.out.println(number);
+        } else{
+            System.out.println("The number "+number +" is less than 20");
+        }
+    }
+}
