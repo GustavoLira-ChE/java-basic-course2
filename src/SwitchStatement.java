9446c36a42f446f5414ee91109e4e23e88a7178d
Gustavo Lira <gu.lirar23@gmail.com>
2022-08-30 12:19:19 -0500

    add: switch case example

---
+++ src/SwitchStatement.java
@@ -0,0 +1,17 @@
+public class SwitchStatement {
+    public static void main(String[] args){
+        String colorModeSelected = "Dark";
+
+        switch (colorModeSelected){
+            case "Light":
+                System.out.println("Seleccionante " + colorModeSelected + " mode");
+                break;
+            case "Dark":
+                System.out.println("Seleccionante " + colorModeSelected + " mode");
+                break;
+            case "Night":
+                System.out.println("Seleccionante " + colorModeSelected + " mode");
+                break;
+        }
+    }
+}
